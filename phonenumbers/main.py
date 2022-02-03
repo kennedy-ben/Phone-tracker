0c6429ff8406a95900cf64627ccc80033c39946a
kennedy bernard <makuanikennedy@gmail.com>
2022-02-03 23:41:49 +0300

    adeded location

--- phonenumbers/main.py
+++ phonenumbers/main.py
@@ -3,6 +3,10 @@ import phonenumbers
 from test import number
 
 from phonenumbers import geocoder
+
+yourLocation = geocoder.description_for_number(phonenumbers,"en")
+print(yourLocation)
+
 ch_number = phonenumbers.parse(number,"CH")
 print(geocoder.description_for_number(ch_number,"en"))
 
